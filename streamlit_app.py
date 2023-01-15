0c2b28006ead2ada248df28ab5a6ff65a18bf858
Akram <119796144+akrammihyar@users.noreply.github.com>
2023-01-15 15:25:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,21 @@
 import pandas as pd
 import streamlit as st
 
-st.markdown("""# What songs were popular when I was in high school?
-The algorithm doesn't get you, we get that a lot. Maybe you want to rediscover the top songs from your high school days. Or maybe you just don't want to mess with making your own playlist. 
-
-You can use this tool to find a pre-generated playlist of every song that made the Top 10 in the US for the years you select. 
-
-This originally appeared on [Datafantic.com](https://www.datafantic.com/what-songs-were-popular-when-i-was-in-high-school).
+st.markdown("""# Date Night
+Test test test ).
 """)
 
-df = pd.read_csv("playlists.csv")
-years = list(range(1958, 2022))
+df = pd.read_csv("questions.csv")
+decks = ['fun', 'date', 'truth']
 
-year_list = st.multiselect(label="Start Year", options=years, default= [1995, 2010])
-
+decks_list = st.multiselect(label="Choose card Deck", options=decks, default= ['fun])
+                                                                               
+selected_decks = st.multiselect(label="Select people", options=people)
 
 if st.button('Play'):
-    if len(year_list) == 1:
-        playlist_name = f"Top US Singles: {year_list[0]}"
+    if len(decks_list) == 1:
+         for decks in selected_decks:
+            id = df.loc[df['deck'] == decks, 'id']
+            st.write(f"Id for {decks} is {id}")
     else:
-        playlist_name = f"Top US Singles: {year_list[0]}-{year_list[1]}"
-
-    if df[df['name'] == playlist_name].shape[0] > 0:
-        playlist = df[df['name'] == playlist_name].to_dict(orient='records')[0]
-    else:
-        playlist = "Ooops, it looks like we didn't make that playlist yet. Playlists with a range of 1-20 years were created. Try again with a more narrow year range."
-
-    if isinstance(playlist, dict):
-        link = f"### Your Spotify Playlist: [{playlist['name']}]({playlist['link']})"
-        st.markdown(link, unsafe_allow_html=True)
-    else:
-        st.markdown(playlist)
+        st.write("Please select at least one deck.")
